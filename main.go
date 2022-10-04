6ba5412bb1e4601e31d9f6b1526fead353df3ee6
evidela96 <evidela@andreani.com>
2022-10-04 11:25:03 -0300

    Publicacion y Consumicion Hecha

--- main.go
+++ main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"sync"
 
 	"github.com/architecture-it/go-platform/AMQStream"
 	"github.com/architecture-it/integracion-schemas-event-go/Onboarding/Events"
 )
 
-type publisherTest struct {
-}
-
-func (p *publisherTest) To(event AMQStream.ISpecificRecord, key string) error {
-	config, err := AMQStream.AddKafka()
-	if err != nil {
-		return err
-	}
-	topics := []string{"PedidoAsignadoTest"}
-	config.ToProducer(event, topics)
-	return nil
-}
+type PublisherTest struct{}
+type SuscriberTest struct{}
 
 func main() {
 	event := Events.Pedido{
@@ -28,56 +20,43 @@ func main() {
 		CicloDelPedido:          "c2781822-5f08-4302-bb9e-8154cc12f43a",
 		CodigoDeContratoInterno: 123123123,
 		CuentaCorriente:         5231235123,
-		Cuando:                  "Nose",
+		Cuando:                  "3/10/2022Z00:00:0000",
 	}
+
+	publish := PublisherTest{}
+	publish.To(&event, "key123")
+
 	config, err := AMQStream.AddKafka()
 	if err != nil {
 		log.Fatal(err)
 	}
-	topics := []string{"PedidoAsignadoTest"}
-	config.ToProducer(&event, topics)
+	suscriptor := SuscriberTest{}
+	consumedEvent := Events.Pedido{}
+	var wg sync.WaitGroup
 
-	err = AMQStream.To(&event, "key123")
+	config.ToConsumer(&suscriptor, &consumedEvent, "PedidoAsignadoTest")
+
+	wg.Add(1)
+	go config.Build()
+	wg.Wait()
+
+}
+
+func (p *PublisherTest) To(event AMQStream.ISpecificRecord, key string) error {
+	config, err := AMQStream.AddKafka()
+	if err != nil {
+		return err
+	}
+	topics := []string{"PedidoAsignadoTest"}
+	config.ToProducer(event, topics)
+	err = AMQStream.To(event, "key123")
 	if err != nil {
 		log.Fatal(err)
 	}
-	//publish.publishTest(event)
-
-	// suscriptor := SuscriberTest{}
-	// config.ToConsumer(suscriptor, &event, "PedidoAsignadoTest")
-	// var wg sync.WaitGroup
-
-	// wg.Add(1)
-	// config.Build()
-	// wg.Wait()
-
+	return nil
 }
 
-/* type publisherTest struct {
+func (s *SuscriberTest) Handler(event AMQStream.ISpecificRecord, metadata AMQStream.ConsumerMetadata) {
+
+	fmt.Printf("Evento: %v\n", event)
 }
-
-type SuscriberTest struct {
-}
-
-func (p publisherTest) publish(event AMQStream.ISpecificRecord, c *AMQStream.Config) {
-
-	c.To(event, "key1234")
-}
-
-func (p publisherTest) publishTest(event Events.Pedido) {
-	config, _ := AMQStream.AddKafka()
-	topics := []string{"PedidoAsignado", "PedidoAsignadoTest"}
-	config.ToProducer(&event, topics)
-
-	config.To(&event, "key123")
-	fmt.Printf("Pedido publicado: %v\n", &event)
-}
-
-func (s SuscriberTest) Handler(event AMQStream.ISpecificRecord, metadata AMQStream.ConsumerMetadata) {
-
-	fmt.Printf("EventoTest PedidoAsignado: %v\n", event)
-}
-
-func (s SuscriberTest) Setup(publisher AMQStream.IPublisher) {
-
-} */
